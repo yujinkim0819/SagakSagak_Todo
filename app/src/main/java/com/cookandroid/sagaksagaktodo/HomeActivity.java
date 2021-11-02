43b53a7b489be1c720bbfdd24f12b72062a538fe
SRin23 <serin4723@naver.com>
2021-11-02 20:50:34 +0900

    serin_fragment detail 및 Home menu 추가

--- app/src/main/java/com/cookandroid/sagaksagaktodo/HomeActivity.java
+++ app/src/main/java/com/cookandroid/sagaksagaktodo/HomeActivity.java
@@ -9,12 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class GraphActivity extends Fragment {
+public class HomeActivity extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
-
-        View v = inflater.inflate(R.layout.activity_graph, container, false);
+        View v = inflater.inflate(R.layout.activity_home, container, false);
         return v;
     }
 }
--- app/src/main/java/com/cookandroid/sagaksagaktodo/MainActivity.java
+++ app/src/main/java/com/cookandroid/sagaksagaktodo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.cookandroid.sagaksagaktodo;
 
 import android.os.Bundle;
+import android.text.Html;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
@@ -8,6 +9,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+//각 프레그먼트의 action bar text변경
+//action bar크기 변경
+//bottom navigation check, uncheck 색깔변경
+//bottom navigation item 1개더 추가(HOME)
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "Main_Activity";
@@ -20,17 +25,24 @@ public class MainActivity extends AppCompatActivity {
 
         mBottomNavigationView = findViewById(R.id.bottom_navigation);
 
-        getSupportFragmentManager().beginTransaction().add(R.id.frame_container, new GraphActivity()).commit();
+        getSupportActionBar().setTitle(Html.fromHtml("<font color=\"black\">" + getString(R.string.title_home) + "</font>"));
+        getSupportFragmentManager().beginTransaction().add(R.id.frame_container, new HomeActivity()).commit();
 
         mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch(item.getItemId()){
+                    case R.id.nav_home :
+                        getSupportActionBar().setTitle(Html.fromHtml("<font color=\"black\">" + getString(R.string.title_home) + "</font>"));
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, new HomeActivity()).commit(); break;
                     case R.id.nav_graph :
+                        getSupportActionBar().setTitle(Html.fromHtml("<font color=\"black\">" + getString(R.string.title_graph) + "</font>"));
                         getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, new GraphActivity()).commit(); break;
                     case R.id.nav_timer :
+                        getSupportActionBar().setTitle(Html.fromHtml("<font color=\"black\">" + getString(R.string.title_timer) + "</font>"));
                         getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, new TimerActivity()).commit(); break;
                     case R.id.nav_todo :
+                        getSupportActionBar().setTitle(Html.fromHtml("<font color=\"black\">" + getString(R.string.title_todo) + "</font>"));
                         getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, new TodoActivity()).commit(); break;
                 }
                 return true;
